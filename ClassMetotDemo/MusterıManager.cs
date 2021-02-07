694fcb644f35d4f1e31f282d41ecd03fd5e85866
hakan budak <hakanbudk1@gmail.com>
2021-02-07 19:52:48 +0300

    Proje dosyası ekle.

---
+++ ClassMetotDemo/MusterıManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusterıManager
+    {
+        public void MusterıEkle(Musterı musterı)
+        {
+            Console.WriteLine(musterı.Ad +musterı.Soyad+" "+"Sisteme Eklenmiştir.");
+        }
+
+        public void Lısteleme(Musterı[] müsteriler)
+        {
+            foreach (Musterı musteri in müsteriler)
+            {
+                Console.WriteLine(musteri.Ad + ":" + musteri.Id);
+            }
+        }
+        public void Musterisilme(Musterı musterısil)
+        {
+            Console.WriteLine(musterısil.Id + "Numaralı " + musterısil.Ad + " Lısteden silinmiştir");
+        }
+        
+
+
+
+        
+    }
+}
+        
+ 
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musterı müşteri = new Musterı();
+            müşteri.Id = 4522;
+            müşteri.Ad = "Hakan";
+            müşteri.Soyad = "Budak";
+
+            Musterı müşteri2 = new Musterı();
+            müşteri2.Id = 1885;
+            müşteri2.Ad = "Ebru";
+            müşteri2.Soyad = "Budak";
+
+            Musterı müşteri3 = new Musterı();
+            müşteri3.Id = 5321;
+            müşteri3.Ad = "Gulden";
+            müşteri3.Soyad = "Budak";
+
+            Musterı müşteri4 = new Musterı();
+            müşteri4.Id = 2121;
+            müşteri4.Ad = "Adem";
+            müşteri4.Soyad = "Budak";
+
+            Musterı[] musteridizi = new Musterı[] { müşteri, müşteri2, müşteri3, müşteri4 };
+            MusterıManager müsterimanager = new MusterıManager();
+            foreach (Musterı musteri in musteridizi)
+            {
+                müsterimanager.MusterıEkle(musteri);
+            }
+
+            Console.WriteLine("--------Listeleme--------");
+            müsterimanager.Lısteleme(musteridizi);
+            Console.WriteLine("--------Silme------------");
+            müsterimanager.Musterisilme(müşteri2);
+
+            Console.Read();
+
+        }
+    }
+}
